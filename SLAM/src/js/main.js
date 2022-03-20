a333809a1a3529a2fd82ba6e0c8bf5ba6f717ced
TatsianaKaleda <t.kaleda@dt-pro.net>
2022-03-21 00:09:19 +0300

    Header,Footer,Modal

--- SLAM/src/js/main.js
+++ SLAM/src/js/main.js
@@ -3,6 +3,7 @@ import '../../node_modules/slick-carousel/slick/slick';
 import '../../node_modules/focus-visible/dist/focus-visible';
 import '../sass/style.scss';
 
+import './components/cleanButton';
 import './components/calculator';
 import './components/smoothScroll';
 import './components/formSubmit';
